52c61e6578727bc8317912016e47663f8287f794
Curtis <curtisdevloper@gmail.com>
2020-05-09 23:58:25 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,26 @@
+-- INSERT INTO (name) bears VALUES ('Mr. Chocolate');
+-- INSERT INTO (name) bears VALUES ('Rowdy');
+-- INSERT INTO (name) bears VALUES ('Tabitha');
+-- INSERT INTO (name) bears VALUES ('Sergeant');
+-- INSERT INTO (name) bears VALUES ('Melissa');
+-- INSERT INTO (name) bears VALUES ('Grinch');
+-- INSERT INTO (name) bears VALUES ('Wendy');
+-- INSERT INTO (name) bears VALUES (null);
+
+-- INSERT INTO (name) bears VALUES (1,'Mr. Chocolate',10,'M','Brown','Mild',1);
+-- INSERT INTO bears VALUES (2,'Rowdy',5,'M','Black','Mild',1);
+-- INSERT INTO bears VALUES (3,'Tabitha',6,'F','White','Mild',1);
+-- INSERT INTO bears VALUES (4,'Sergeant',6,'M','Brown','Mild',1);
+-- INSERT INTO bears VALUES (5,'Melissa',7,'F','Black','Mild',1);
+-- INSERT INTO bears VALUES (6,'Grinch',2,'M','Brown','Aggressive',1);
+-- INSERT INTO bears VALUES (7,'Wendy',6,'F','Brown','Aggressive',1);
+-- INSERT INTO bears VALUES (8,null,6,'F','White','Aggressive',1);
+
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
